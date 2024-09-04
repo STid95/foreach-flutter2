00bd4c4f0da9021dc5fc9dc5d200283a5ebe4130
STid95 <noemie.contant@symbol-it.fr>
2024-09-04 10:58:47 +0200

    textformfield + switch + style

--- lib/views/add_page/add_page.dart
+++ lib/views/add_page/add_page.dart
@@ -14,7 +14,7 @@ class AddPage extends StatefulWidget {
 class _AddPageState extends State<AddPage> {
   final gameNameController = TextEditingController();
   GameType? dropdownValue = GameType.RPG;
-  bool isFamilyFriendly = true;
+  bool isPerfect = false;
 
   @override
   Widget build(BuildContext context) {
@@ -23,91 +23,148 @@ class _AddPageState extends State<AddPage> {
     return VGScaffold(
         title: widget.title,
         body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(20),
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      Row(children: [
-                        const Text('Nom du jeu :'),
-                        const SizedBox(
-                          width: 10.0,
+            child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(children: [
+            Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Theme.of(context).primaryColor, width: 2.0),
                         ),
-                        Expanded(
-                          child: TextFormField(
-                            autovalidateMode: AutovalidateMode.onUserInteraction,
-                            onChanged: (value) {
-                              if (_formKey.currentState!.validate()) {
-                                Navigator.of(context).pop();
-                              }
-                              ;
-                            },
-                            controller: gameNameController,
-                            validator: (value) {
-                              if (value == null || value.length < 2) {
-                                return "Vous devez entrer un nom";
-                              } else
-                                return null;
-                            },
-                          ),
-                        ),
-                      ]),
-                    ],
-                  ),
-                ),
-              ),
-              DropdownButton(
-                value: dropdownValue,
-                items: GameType.values.map((e) => DropdownMenuItem(value: e, child: Text(e.name))).toList(),
-                onChanged: (value) {
-                  setState(() {
-                    dropdownValue = value;
-                  });
-                },
-              ),
-              Switch(
-                  trackOutlineColor: WidgetStateProperty.resolveWith(
-                    (final Set<WidgetState> states) {
-                      if (states.contains(WidgetState.selected)) {
+                        border: const UnderlineInputBorder(),
+                        iconColor: Theme.of(context).primaryColor,
+                        icon: Icon(Icons.games_rounded),
+                        labelText: 'Nom du jeu',
+                        errorStyle: TextStyle(color: Colors.red)),
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    onChanged: (value) {
+                      if (_formKey.currentState!.validate()) {
+                        //Navigator.of(context).pop();
+                      }
+                    },
+                    controller: gameNameController,
+                    validator: (value) {
+                      if (value == null || value.length < 2) {
+                        return "Vous devez entrer un nom";
+                      } else {
                         return null;
                       }
-                      return Colors.black;
                     },
                   ),
-                  inactiveTrackColor: Colors.white,
-                  inactiveThumbColor: Theme.of(context).primaryColor,
-                  activeColor: Colors.white,
-                  value: isFamilyFriendly,
-                  onChanged: (value) => setState(() {
-                        isFamilyFriendly = value;
-                      })),
-              Checkbox(
-                  value: isFamilyFriendly,
-                  onChanged: (value) => setState(() {
-                        isFamilyFriendly = value ?? false;
-                      })),
-              Radio(
-                value: true,
-                onChanged: (value) => setState(() {
-                  isFamilyFriendly = value ?? false;
-                }),
-                groupValue: isFamilyFriendly,
+                  Flex(
+                    direction: Axis.horizontal,
+                    children: [
+                      const Flexible(
+                        flex: 2,
+                        fit: FlexFit.loose,
+                        child: Text('Nom du jeu :'),
+                      ),
+                      const Spacer(),
+                      Flexible(
+                        flex: 6,
+                        fit: FlexFit.tight,
+                        child: TextFormField(
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          onChanged: (value) {
+                            if (_formKey.currentState!.validate()) {
+                              //Navigator.of(context).pop();
+                            }
+                          },
+                          controller: gameNameController,
+                          validator: (value) {
+                            if (value == null || value.length < 2) {
+                              return "Vous devez entrer un nom";
+                            } else {
+                              return null;
+                            }
+                          },
+                        ),
+                      )
+                    ],
+                  ),
+                ],
               ),
-              TextButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text(gameNameController.value.text)),
-                    );
-                  }
-                },
-                child: Text('Récupérer'),
-              )
-            ],
-          ),
-        ));
+            ),
+            Flex(
+              direction: Axis.horizontal,
+              children: [
+                const Flexible(
+                  flex: 2,
+                  fit: FlexFit.tight,
+                  child: Text('Style de jeu :'),
+                ),
+                const Spacer(),
+                Flexible(
+                  fit: FlexFit.loose,
+                  flex: 6,
+                  child: DropdownButton(
+                    value: dropdownValue,
+                    items: GameType.values.map((e) => DropdownMenuItem(value: e, child: Text(e.name))).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        dropdownValue = value;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Flex(
+              direction: Axis.horizontal,
+              children: [
+                const Flexible(flex: 2, fit: FlexFit.tight, child: Text('Parfait ?')),
+                const Spacer(),
+                Flexible(
+                  fit: FlexFit.loose,
+                  flex: 6,
+                  child: Switch(
+                      trackOutlineColor: WidgetStateProperty.resolveWith(
+                        (final Set<WidgetState> states) {
+                          if (states.contains(WidgetState.selected)) {
+                            return null;
+                          }
+                          return Colors.black;
+                        },
+                      ),
+                      inactiveTrackColor: Colors.white,
+                      inactiveThumbColor: Theme.of(context).primaryColor,
+                      activeColor: Colors.white,
+                      value: isPerfect,
+                      onChanged: (value) => setState(() {
+                            isPerfect = value;
+                          })),
+                ),
+                /* ]),
+                Checkbox(
+                    value: isFamilyFriendly,
+                    onChanged: (value) => setState(() {
+                          isFamilyFriendly = value ?? false;
+                        })),
+                Radio(
+                  value: true,
+                  onChanged: (value) => setState(() {
+                    isFamilyFriendly = value ?? false;
+                  }),
+                  groupValue: isFamilyFriendly,
+                ),*/
+              ],
+            ),
+            TextButton(
+              onPressed: () {
+                if (_formKey.currentState != null && _formKey.currentState!.validate()) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text(gameNameController.value.text)),
+                  );
+                }
+              },
+              child: Text('Ajouter'),
+            )
+          ]),
+        )));
   }
 }
